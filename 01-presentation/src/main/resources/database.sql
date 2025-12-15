51aa98a61cb7c2ad53af09bb4bf208a416f9dbf7
AngelDiazProgrammer <124737478+AngelDiazProgrammer@users.noreply.github.com>
2025-12-14 20:57:56 -0500

    Update database.sql

--- 01-presentation/src/main/resources/database.sql
+++ 01-presentation/src/main/resources/database.sql
@@ -193,7 +193,7 @@ INSERT INTO dom_servicios (nombre_servicio, descripcion, precio) VALUES
 ('SPA', 'Spa y masajes', 50.00);
 
 INSERT INTO usuarios (nombre_usuario, contrasena, nombres, apellidos, email, id_rol) VALUES
-('admin', '$2a$12$VFnHigzjeYomhCrOHjmwxuLT9.RcD5.A4OrZy8mNh0zQcCf5XB1dy', 'Admin', 'Sistema', 'admin@hotelamericas.com', 1),
+('admin', '123456', 'Admin', 'Sistema', 'admin@hotelamericas.com', 1),
 ('sara', '$2a$12$VFnHigzjeYomhCrOHjmwxuLT9.RcD5.A4OrZy8mNh0zQcCf5XB1dy', 'Sara', 'Martinez', 'sara@hotelamericas.com', 2),
 ('ema', '$2a$12$VFnHigzjeYomhCrOHjmwxuLT9.RcD5.A4OrZy8mNh0zQcCf5XB1dy', 'Ema', 'Rodriguez', 'ema@hotelamericas.com', 2),
 ('angel', '$2a$12$VFnHigzjeYomhCrOHjmwxuLT9.RcD5.A4OrZy8mNh0zQcCf5XB1dy', 'Angel', 'Lopez', 'angel@hotelamericas.com', 2),
